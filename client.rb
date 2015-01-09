0a384d12edaac8b7b53fa89a9994fcefd00b7b0c
Veselin Dobrev <baiumbg@gmail.com>
2015-01-09 16:41:14 +0200

    Added email download to constructor

--- client.rb
+++ client.rb
@@ -6,24 +6,45 @@ module RubyMail
     attr_reader :user
 
     def initialize(username = nil)
-      if(username.end_with? "@gmail.com") then username.insert(0, "recent:") end
-      database = SQLite3::Database.new("data.db")
-      database.execute "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT)"
-      database.execute "CREATE TABLE IF NOT EXISTS emails(id INTEGER PRIMARY KEY AUTOINCREMENT, message_id TEXT, text TEXT, html TEXT, from TEXT, to TEXT, subject TEXT, received TEXT, attachment)"
-      database.execute("SELECT COUNT(1) FROM users WHERE email = @username", username) do |count|
-        if count == [0] then database.execute("INSERT INTO users VALUES(NULL, @username)", username) end
-      end
       @user = username
+      database = SQLite3::Database.new("data.db")
+      database.execute "CREATE TABLE IF NOT EXISTS users(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `email` TEXT, `password` TEXT)"
+      database.execute "CREATE TABLE IF NOT EXISTS emails(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `message_id` TEXT, `text` TEXT, html TEXT, `from` TEXT, `to` TEXT, `subject` TEXT, `received` TEXT)"
+      database.execute "CREATE TABLE IF NOT EXISTS attachments(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `message_id` TEXT, `file_name` TEXT, `attachment` BLOB)"
+      result = database.execute("SELECT * FROM `users` WHERE `email` = @username", username)
+        #if result.count == 0 then database.execute("INSERT INTO users VALUES(NULL, @username)", username) end
+  		Mail.defaults do
+  		  if(username.end_with? "@gmail.com") then username.insert(0, "recent:") end
+  		  retriever_method :pop3, :address    => "pop.gmail.com",
+  		                          :port       => 995,
+  		                          :user_name  => username,
+  		                          :password   => result[0][2],
+  		                          :enable_ssl => true
+  		end
+      Mail.all.each do |mail|
+        database.execute(
+          "IF NOT EXISTS (SELECT * FROM emails WHERE `email` = '@username' AND `message_id` = '@id') INSERT INTO emails VALUES(NULL, @id, @text, @html, @from, @to, @subject, @received)",
+          @user,
+          mail.message_id,
+          mail.text_part,
+          mail.html_part,
+          mail.from,
+          mail.to,
+          mail.subject,
+          mail.date
+        )
+        mail.attachments.each do |attachment|
+          database.execute(
+            "IF NOT EXISTS (SELECT * FROM attachments WHERE `message_id` = '@id')
+            INSERT INTO attachments VALUES(NULL, @id, @file_name, @attachment",
+            mail.message_id,
+            attachment.filename,
+            attachment.body.decoded
+          )
+        end
+      end
     end
   end
-end
-
-Mail.defaults do
-  retriever_method :pop3, :address    => "pop.gmail.com",
-                          :port       => 995,
-                          :user_name  => 'recent:',
-                          :password   => '',
-                          :enable_ssl => true
 end
 
 # Mail.all.each do |email|
@@ -40,3 +61,6 @@ end
 #     puts "Saved #{attachment.filename}"
 #   end
 # end
+
+test = RubyMail::Client.new("baiumbg@gmail.com")
+#p Mail.last.subject
