5decffa3a8b05e8310be121baa6a2693e4118b44
Veselin Dobrev <baiumbg@gmail.com>
2015-02-07 14:22:00 +0200

    Split constructor into seperate methods
    Split the constructor into seperate methods for database and mailer initialization
    Also added a method for mail syncing.

--- client.rb
+++ client.rb
@@ -3,40 +3,67 @@ require 'mail'
 
 module RubyMail
   class Client
-    attr_reader :user
+    def initialize(email, password, pop3, pop3_port, ssl, smtp, smtp_port)
+      init_db
+      init_mail(email, password, pop3, pop3_port, ssl, smtp, smtp_port)
+      sync_mail email
+      #puts "#{unread.count} unread emails"
+    end
 
-    def initialize(username = nil)
-      @user = username
-      database = SQLite3::Database.new("data.db")
-      database.execute "CREATE TABLE IF NOT EXISTS users(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `email` TEXT, `password` TEXT)"
-      database.execute "CREATE TABLE IF NOT EXISTS emails(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `message_id` TEXT, `text` TEXT, html TEXT, `from` TEXT, `to` TEXT, `subject` TEXT, `received` TEXT)"
-      database.execute "CREATE TABLE IF NOT EXISTS attachments(`id` INTEGER PRIMARY KEY AUTOINCREMENT, `message_id` TEXT, `file_name` TEXT, `attachment` BLOB)"
-      result = database.execute("SELECT * FROM `users` WHERE `email` = @username", username)
-        #if result.count == 0 then database.execute("INSERT INTO users VALUES(NULL, @username)", username) end
-  		Mail.defaults do
-  		  if(username.end_with? "@gmail.com") then username.insert(0, "recent:") end
-  		  retriever_method :pop3, :address    => "pop.gmail.com",
-  		                          :port       => 995,
-  		                          :user_name  => username,
-  		                          :password   => result[0][2],
-  		                          :enable_ssl => true
-  		end
-      Mail.all.each do |mail|
-        database.execute(
-          "IF NOT EXISTS (SELECT * FROM emails WHERE `email` = '@username' AND `message_id` = '@id') INSERT INTO emails VALUES(NULL, @id, @text, @html, @from, @to, @subject, @received)",
-          @user,
+    def init_db
+      @database = SQLite3::Database.new("data.db")
+      @database.execute "CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, password TEXT, pop3 TEXT, pop3_port INTEGER, smtp TEXT, smtp_port INTEGER, ssl INTEGER)"
+      @database.execute "CREATE TABLE IF NOT EXISTS emails(id INTEGER PRIMARY KEY AUTOINCREMENT, message_id TEXT, text TEXT, html TEXT, [from] TEXT, [to] TEXT, subject TEXT, received TEXT, read INTEGER)"
+      @database.execute "CREATE TABLE IF NOT EXISTS attachments(id INTEGER PRIMARY KEY AUTOINCREMENT, message_id TEXT, file_name TEXT, attachment BLOB)"
+    end
+
+    def init_mail(email, password, pop3, pop3_port, ssl, smtp, smtp_port)
+      Mail.defaults do
+        if(email.end_with? "@gmail.com") then username.insert(0, "recent:") end
+        retriever_method :pop3, :address    => pop3,
+                                :port       => pop3_port,
+                                :user_name  => email,
+                                :password   => password,
+                                :enable_ssl => ssl
+
+        delivery_method :smtp, :address                => smtp,
+                               :port                   => smtp_port,
+                               :domain                 => smtp,
+                               :user_name              => email,
+                               :password               => password,
+                               :authentication         => 'plain',
+                               :enable_start_ttls_auto => true
+
+        @unread = []
+      end
+    end
+
+    def sync_mail(email)
+      last_synced = @database.execute("SELECT message_id FROM emails WHERE id = (SELECT MAX(id) FROM emails WHERE [to] = @username)", email).flatten[0]
+      new_mail = []
+      if(last_synced == nil)
+        new_mail = Mail.all
+      else
+        Mail.all.reverse.each do |mail|
+          if(mail.message_id != last_synced) then new_mail.push(mail) end
+          break if mail.message_id == last_synced
+        end
+        new_mail.reverse!
+      end
+      new_mail.each do |mail|
+        @database.execute(
+          "INSERT INTO emails VALUES(NULL, @id, @text, @html, @from, @to, @subject, @received, 0)",
           mail.message_id,
-          mail.text_part,
-          mail.html_part,
+          mail.text_part.decoded,
+          mail.html_part.decoded,
           mail.from,
           mail.to,
           mail.subject,
-          mail.date
+          mail.date.to_s
         )
         mail.attachments.each do |attachment|
-          database.execute(
-            "IF NOT EXISTS (SELECT * FROM attachments WHERE `message_id` = '@id')
-            INSERT INTO attachments VALUES(NULL, @id, @file_name, @attachment",
+          @database.execute(
+            "INSERT INTO attachments VALUES(NULL, @id, @file_name, @attachment)",
             mail.message_id,
             attachment.filename,
             attachment.body.decoded
@@ -44,23 +71,11 @@ module RubyMail
         end
       end
     end
+    
+    def unread
+      @database.execute("SELECT * FROM emails WHERE read = 0")
+    end
   end
 end
 
-# Mail.all.each do |email|
-#   email.parts.select { |part| part.content_type.include? "text/plain" }.each do |part|
-#     puts part.body.decoded
-#     puts "--------------"
-#   end
-# end
-# Mail.last.attachments.each do |attachment|
-#   if(File.file? "attachments/#{attachment.filename}")
-#     puts "There is already a file called #{attachment.filename} in the attachments folder."
-#   else
-#     File.write("attachments/#{attachment.filename}", attachment.body.decoded)
-#     puts "Saved #{attachment.filename}"
-#   end
-# end
-
-test = RubyMail::Client.new("baiumbg@gmail.com")
-#p Mail.last.subject
+test = RubyMail::Client.new("", "", "pop3.abv.bg", 995, true, "smtp.abv.bg", 465)
