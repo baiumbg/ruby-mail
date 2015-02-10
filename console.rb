290001a27e6a2add7809928b0fa170a8522106fc
Veselin Dobrev <baiumbg@gmail.com>
2015-02-10 18:26:28 +0200

    Initial shell commit

--- console.rb
+++ console.rb
@@ -7,6 +7,7 @@ module RubyMail
     def initialize
       login
       start_sync
+      start_shell
     end
 
     def login
@@ -17,17 +18,52 @@ module RubyMail
       else
         print "Password: "
         password = STDIN.noecho(&:gets).chomp
+
         print "\nPOP3 Server: "
         pop3 = gets.chomp
+
         print "POP3 Server Port: "
         pop3_port = gets.chomp.to_i
+
         print "Enable SSL? (0 or 1): "
-        ssl = gets.chomp.to_i
+        pop3_ssl = gets.chomp.to_i
+
         print "SMTP Server: "
         smtp = gets.chomp
+
         print "SMTP Server Port: "
         smtp_port = gets.chomp.to_i
-        @client = Client.new(user_name, password, pop3, pop3_port, ssl, smtp, smtp_port)
+
+        print "Enable SSL? (0 or 1): "
+        smtp_ssl = gets.chomp.to_i
+
+        @client = Client.new(user_name, password, pop3, pop3_port, pop3_ssl, smtp, smtp_port, smtp_ssl)
+      end
+    end
+
+    def start_shell
+      system "cls" or system "clear"
+      cmd = ""
+      while(cmd != "quit") do
+        print "#{@client.email}$ "
+        cmd = gets.chomp
+        process_cmd cmd
+      end
+    end
+
+    def process_cmd(cmd)
+      args = cmd.split ' '
+      case args[0]
+      when "read"
+        if(args.length > 2) 
+          puts "Too many arguments."
+        elsif(args.length < 2)
+          puts "Missing arguments."
+        else
+          read(args[1].to_i)
+        end
+      else
+        puts "Unknown command."
       end
     end
 
@@ -42,5 +78,4 @@ module RubyMail
   end
 end
 
-RubyMail::ConsoleInterface.new
-gets
+test = RubyMail::ConsoleInterface.new
