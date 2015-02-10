8040d416a9d58e82a0f2f7e41175540dc719e151
Veselin Dobrev <baiumbg@gmail.com>
2015-02-10 18:27:51 +0200

    Fixed user_exists? and added various other utilities
    Fixed user_exists? for when the database has not been initialized yet.
    Added mail fetching by id and message_id.

--- utils.rb
+++ utils.rb
@@ -4,7 +4,14 @@ module RubyMail
   class Utilities
     @@database = SQLite3::Database.new("data.db")
     def self.user_exists?(user_name)
-      !@@database.execute("SELECT * FROM users WHERE email = @user_name", user_name).flatten.empty?
+      @@database.execute("SELECT * FROM sqlite_master WHERE name='users' AND type='table'").flatten.any? and
+      @@database.execute("SELECT * FROM users WHERE email = ?", user_name).flatten.any?
+    end
+    def self.get_mail_by_id(id)
+      @@database.execute("SELECT * FROM emails WHERE id = ?", id).flatten[0]
+    end
+    def self.get_mail_by_message_id(message_id)
+      @@database.execute("SELECT * FROM emails WHERE message_id = ?", message_id).flatten[0]
     end
   end
 end
